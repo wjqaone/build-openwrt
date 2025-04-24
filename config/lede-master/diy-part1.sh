643b719c404fd03eb75a1a58c077e7d25c3b5575
wjqaone <wjqaone@gmail.com>
2025-04-24 16:03:20 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -11,6 +11,8 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+sed -i '$a src-git lwb1978 https://github.com/lwb1978/openwrt-gecoosac' feeds.conf.default
+sed -i '$a src-git brvphoenix https://github.com/brvphoenix/luci-app-wrtbwmon' feeds.conf.default
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
